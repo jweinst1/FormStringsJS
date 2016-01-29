47b9d1ba7c2a621c36482d729df556178533752a
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-01-29 10:07:50 -0800

    First Commit

---
+++ Formats.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Josh on 1/28/16.
+ */
+var findvars = (function () {
+    function findvars() {
+    }
+    //finds number of vars
+    findvars.countvars = function (input) {
+        var matches = input.match(/\{[^ \{\}]\}/g);
+        return matches.length;
+    };
+    //finds all vars in string
+    findvars.getvars = function (input) {
+        return input.match(/\{[^ \{\}]\}/g);
+    };
+    //checks for duplicate variables in the string
+    findvars.checkduplicates = function (input) {
+        var found = findvars.getvars(input);
+        var counts = {};
+        for (var i = 0; i < found.length; i++) {
+            if (found[i] in counts)
+                return false;
+            else
+                counts[found[i]] = 1;
+        }
+        return true;
+    };
+    return findvars;
+})();
+var replacevars = (function () {
+    function replacevars() {
+    }
+    replacevars.replacevar = function (input, keys) {
+        for (var key in keys) {
+            var patt = new RegExp("{" + key + "}");
+            input = input.replace(patt, keys[key]);
+        }
+        return input;
+    };
+    return replacevars;
+})();
+
+//main function for template strings
+function FormString(string, obj) {
+    return replacevars.replacevar(string, obj);
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+#FormStringsJS
+
+A simple addon for Node to make easy Template Strings in JavaScript.
+
+##Installation
+
+To Install FormStringsJS, simply type in your shell:
+
+`npm install formstringsjs`
+
+##Usage
+
+To use the package, you have to require it, then use the FormString() function to transform your strings.
+
